252491a41164014348fe2e9d71736e785338b9c4
mohamedahmadazmy319-arch <mohamedahmadazmy319@gmail.com>
2026-08-17 12:47:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function sendWhatsApp() {
-    let name = document.getElementById("name").value;
-    let phone = document.getElementById("phone").value;
-    let message = document.getElementById("message").value;
+    const name = document.getElementById("name").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const message = document.getElementById("message").value.trim();
 
-    let text = `الاسم: ${name}
+    if (!name || !phone || !message) {
+        alert("من فضلك اكتب الاسم ورقم الهاتف والرسالة.");
+        return;
+    }
+
+    const text = `مرحبًا، يوجد عميل مهتم بمشروع TRIVIA MALL
+
+الاسم: ${name}
 رقم الهاتف: ${phone}
 الرسالة: ${message}`;
 
-    let url = "https://wa.me/201070343434?text=" + encodeURIComponent(text);
+    const url =
+        "https://wa.me/201070343434?text=" +
+        encodeURIComponent(text);
 
-    window.location.href = url;
+    window.open(url, "_blank");
+}
+function openImage(src) {
+    document.getElementById("bigImage").src = src;
+    document.getElementById("imageModal").style.display = "flex";
+}
+
+function closeImage() {
+    document.getElementById("imageModal").style.display = "none";
 }
